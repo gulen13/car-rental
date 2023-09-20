cc197476e9820a9cde5d97a6dc4a845dd17bcd09
gulen13 <vitaliy.gulen@gmail.com>
2023-09-20 21:35:35 +0300

    fixed problem with hero image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,12 @@
 import { RentalBtn } from 'components/Buttons/Buttons';
 import React from 'react';
 import { HeroContainer, HeroDescription, HeroImg } from './Hero.styled';
+import hero from '../../images/hero-img.jpg';
 
 const Hero = () => {
   return (
     <HeroContainer>
-      <HeroImg
-        src="https://pixabay.com/get/g5ccc733f921b3c6098ccff63493780e0965299b4d8e369061c081b1369534ecbb7726dade21841659e27bbed30be60c6139e997feb335971e4af2af07bb902f2_1280.jpg"
-        alt="rent a car"
-      />
+      <HeroImg src={hero} alt="rent a car" />
       <HeroDescription>
         <h1>Welcome to Car Rental Hub</h1>
         <h3>
